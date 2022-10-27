0ba95faf32ce16c8e2855e8cadf4e00adf134b43
TyroneHua <huatyrone@gmail.com>
2022-10-27 13:26:48 +0800

    Update TippingPoint.java
    jb

--- TeamA/src/main/java/org/firstinspires/ftc/teamcode/TippingPoint.java
+++ TeamA/src/main/java/org/firstinspires/ftc/teamcode/TippingPoint.java
@@ -38,7 +38,7 @@ public class TippingPoint extends LinearOpMode {
         double liftInitPos = lift.getCurrentPosition();
 
         waitForStart();
-        final double liftSpeed=1;
+        final double liftSpeed = 1;
         double driveMult = 0.5;
         Toggle slowToggle = new Toggle();
         // set default speed to slow
@@ -55,7 +55,7 @@ public class TippingPoint extends LinearOpMode {
 
             double drive = gamepad1.left_stick_y;
             double turn = (gamepad1.right_stick_x == 0) ? -gamepad1.left_stick_x
-                                                        : -gamepad1.right_stick_x;
+                    : -gamepad1.right_stick_x;
             leftPower = Range.clip(drive + turn, -1.0, 1.0) * driveMult;
             rightPower = Range.clip(drive - turn, -1.0, 1.0) * driveMult;
 
@@ -65,49 +65,47 @@ public class TippingPoint extends LinearOpMode {
             rightF.setPower(rightPower);
 
 
-            double v=0.42;
+            double v = 0.42;
 
             // lift down
-            if (gamepad1.left_trigger>0 && lift.getCurrentPosition()<liftInitPos-1000) {
+            if (gamepad1.left_trigger > 0 && lift.getCurrentPosition() < liftInitPos - 1000) {
                 lift.setPower(liftSpeed);
             }
             // lift up
-            else if(gamepad1.right_trigger>0 && lift.getCurrentPosition()>liftInitPos-15800){
+            else if (gamepad1.right_trigger > 0 && lift.getCurrentPosition() > liftInitPos - 15800) {
 
-            // lift downwards movement is positive
-            if (gamepad1.left_trigger>0 && (lift.getCurrentPosition()<liftInitPos-750 || !liftLimitToggle.getState())) {
-                lift.setPower(liftSpeed);
-            }
-            // lift upwards movement is negative, downwards is positive
-            else if(gamepad1.right_trigger>0 && (lift.getCurrentPosition()>liftInitPos-15800 || !liftLimitToggle.getState())){
+                // lift downwards movement is positive
+                if (gamepad1.left_trigger > 0 && (lift.getCurrentPosition() < liftInitPos - 750 || !liftLimitToggle.getState())) {
+                    lift.setPower(liftSpeed);
+                }
+                // lift upwards movement is negative, downwards is positive
+                else if (gamepad1.right_trigger > 0 && (lift.getCurrentPosition() > liftInitPos - 15800 || !liftLimitToggle.getState())) {
 
-                lift.setPower(-liftSpeed);
-            }
-            else{
-                lift.setPower(0);
-            }
+                    lift.setPower(-liftSpeed);
+                } else {
+                    lift.setPower(0);
+                }
 
-            if(gamepad1.left_bumper){
-                servoMotor.setPosition(v);
-            }
-            else if(gamepad1.right_bumper){
-                servoMotor.setPosition(0);
-            }
+                if (gamepad1.left_bumper) {
+                    servoMotor.setPosition(v);
+                } else if (gamepad1.right_bumper) {
+                    servoMotor.setPosition(0);
+                }
 
-            slowToggle.update(this.gamepad1.a);
-            liftLimitToggle.update(this.gamepad1.x);
+                slowToggle.update(this.gamepad1.a);
+                liftLimitToggle.update(this.gamepad1.x);
 
-            if(slowToggle.getState()) {
-                driveMult = 0.5;
-            }
-            else {
-                driveMult = 0.1;
-            }
+                if (slowToggle.getState()) {
+                    driveMult = 0.5;
+                } else {
+                    driveMult = 0.1;
+                }
 
-            telemetry.addData("Lift Position", lift.getCurrentPosition());
-            telemetry.addData("Slow Mode (A):", slowToggle.getState() ? "ON" : "OFF");
-            telemetry.addData("Lift Limit (X):", liftLimitToggle.getState() ? "ON" : "OFF");
-            telemetry.update();
+                telemetry.addData("Lift Position", lift.getCurrentPosition());
+                telemetry.addData("Slow Mode (A):", slowToggle.getState() ? "ON" : "OFF");
+                telemetry.addData("Lift Limit (X):", liftLimitToggle.getState() ? "ON" : "OFF");
+                telemetry.update();
+            }
         }
     }
 }
